f76a7a48a998602c025d42e60bf571549161aa08
mxjmjl <34787881+mxjmjl@users.noreply.github.com>
2023-11-12 16:15:21 +0800

    更新 测试源.js

--- 测试源.js
+++ 测试源.js
@@ -11,7 +11,7 @@ var rule = {
 		'User-Agent': 'MOBILE_UA'
 	},
 	timeout:5000,//网站的全局请求超时,默认是3000毫秒
-	class_name:'央视&卫视频道&港澳台频道&国外电视台&广东&湖南&江苏&安徽&浙江&北京&辽宁&江西&山东&黑龙江&上海&云南&四川&河南&湖北&福建&重庆&河北&吉林&广西&山西&陕西&宁夏&海南&甘肃&新疆&内蒙古&天津&贵州&青海&西藏',
+	class_name:'央視&卫视频道&港澳台频道&国外电视台&广东&湖南&江苏&安徽&浙江&北京&辽宁&江西&山东&黑龙江&上海&云南&四川&河南&湖北&福建&重庆&河北&吉林&广西&山西&陕西&宁夏&海南&甘肃&新疆&内蒙古&天津&贵州&青海&西藏',
 	class_url:'1&2&4&5&3/guangdong&3/hunan&3/jiangsu&3/anhui&3/zhejiang&3/beijing&3/liaoning&3/jiangxi&3/shandong&3/heilongjiang&3/shanghai&3/yunnan&3/sichuan&3/henan&3/hubei&3/fujian&3/zhongqing&3/hebei&3/jilin&3/guangxi&3/shan-xi&3/shanxi&3/ningxia&3/hainan&3/gansu&3/xinjiang&3/neimenggu&3/tianjin&3/guizhou&3/qinghai&3/xizang',
 	play_parse:true,
 	// lazy:'js:input=input.replace("tv", "m")',
