c65325657ec33c2fb78ebecc22572cf66bfb0e90
mxjmjl <34787881+mxjmjl@users.noreply.github.com>
2023-12-19 22:17:02 +0800

    更新 wogg.js

--- wogg.js
+++ wogg.js
@@ -1,22 +1,68 @@
-var rule={
-    title:'玩偶哥哥',
-    模板:'AlistShare',
-    host:'https://wogg.link',
-    url:'/index.php/vodshow/fyclass--hits------fypage---.html',
-    filterable:0,//是否启用分类筛选,
-    filter_url:'',
-    filter: {},
-    searchUrl:'/index.php/vodsearch/**-------------.html',
-    searchable:2,
-    class_parse: 'ul.nav-menu-items&&li;a&&Text;a&&href;/(\\d+).html',
-    推荐:'.module-list;.module-items&&.module-item;*;*;*;*',
-    double:true,
-    一级:'.module-list&&.module-items&&.module-item;.module-item-cover&&.module-item-pic&&a&&title;.module-item-cover&&.module-item-pic&&img&&data-src;.module-item-text&&Text;.module-item-cover&&.module-item-pic&&a&&href',
-    二级:{
-        "title": "h1&&Text;.video-info-aux a:eq(0)&&Text",
-        "img": ".video-cover img&&data-src",
-        "desc": ".module-row-title&&p&&Text;.video-info-main&&.video-info-item:eq(2) a&&Text;.video-info-aux a:eq(3)&&Text;.video-info-main&&.video-info-item:eq(1)&&Text;.video-info-main&&.video-info-item:eq(0)&&Text",
-        "content": ".sqjj_a&&Text",
-    },
-    搜索:'.module-search-item;h3&&Text;*;.video-serial&&Text;.video-info-header&&h3&&a&&href',
+var rule = {
+    title: '玩偶哥哥',
+    host: 'http://home.jundie.top:9520',
+    homeUrl: '/spider/csp_Wogg?ac=detail',
+    searchUrl: '/spider/csp_Wogg?ac=detail&wd=**&pg=fypage',
+    detailUrl: '/spider/csp_Wogg?ac=detail&ids=fyid', //非必填,二级详情拼接链接
+    searchable: 2,
+    quickSearch: 0,
+    filterable: 1,
+    play_parse: false,
+    lazy: '',
+    multi: 1,
+    timeout: 5000,
+    limit: 6,
+    url: '/spider/csp_Wogg?ac=detail&t=fyclass&pg=fypage&f=',
+    class_name: '电影&剧集&动漫&综艺&音乐',
+    class_url: '1&2&3&4&5',
+    play_parse:true,
+    lazy:`js:
+    input = alistPlay(input,playObj.flag)
+    `,
+    推荐: 'json:list;vod_name;vod_pic;vod_remarks;vod_id',
+    一级: 'json:list;vod_name;vod_pic;vod_remarks;vod_id',
+    二级访问前: `js:
+        url = rule.detailUrl.replaceAll('fyid', detailUrl).replaceAll('fyclass', fyclass);
+        MY_URL = urljoin(rule.homeUrl, url);
+    `,
+    二级: `js:
+        let html = request(input);
+        let list = JSON.parse(html).list;
+        if(list.length===1){
+           var vod = list[0];
+           var shareUrl = vod.vod_content;
+           let parts = shareUrl.split("\\n");
+           if (parts.length > 1) {
+            shareUrl = parts[1].trim();
+            shareUrl = shareUrl.replace("www.alipan.com","www.aliyundrive.com")
+            vod.vod_remarks = shareUrl;
+            vod.vod_actor = ""
+            vod.vod_pic = ""
+            initAlistShare();
+            let alistVod = alistDetailContent(vod ,[shareUrl]);
+            TABS = alistVod.tabs
+            LISTS = alistVod.lists
+            vod["vod_play_from"]=alistVod.tabs.join("$$$");
+
+            for (var i in LISTS) {
+                if (LISTS.hasOwnProperty(i)) {
+                // print(i);
+                try {
+                    LISTS[i] = LISTS[i].map(function (it) {
+                    return it.split('$').slice(0, 2).join('$');
+                    });
+                } catch (e) {
+                    print('格式化LISTS发生错误:' + e.message);
+                }
+                }
+            }
+            vod_play_url = LISTS.map(function (it) {
+                return it.join('#');
+            }).join("$$$");
+            vod["vod_play_url"]=vod_play_url;
+           }
+            VOD = vod;
+        }
+    `,
+    搜索: 'json:list;vod_name;vod_pic;vod_remarks;vod_id'
 }
